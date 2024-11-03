3ec46996f62b67eebecb3bd70ce30e61b70513b6
Nadia <nad21lc@gmail.com>
2024-11-03 16:46:37 -0500

    feat: added deployment variables

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine/java:22-jdk
 VOLUME /tmp
-COPY target/*.jar app.jar
+COPY target/backend-0.0.1-SNAPSHOT.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
 EXPOSE 8080
---
+++ src/test/java/b4u/pocketpartners/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package b4u.pocketpartners.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
